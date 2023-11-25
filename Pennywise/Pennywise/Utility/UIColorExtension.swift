48343c7ae2552e01d9c73e8a3a98931b1db41572
shaival-vora <shaivalvora@gmail.com>
2023-11-25 16:31:44 -0500

    Added UIColor Extension class which handles all the custom color for the application

--- Pennywise/Pennywise/Utility/UIColorExtension.swift
+++ Pennywise/Pennywise/Utility/UIColorExtension.swift
@@ -5,4 +5,38 @@
 //  Created by Shaival Vora on 11/25/23.
 //
 
-import Foundation
+import UIKit
+// This is the extension for UIKit UIColor, here we can define custom color palette and it has a custom func do send Hex Color Codes
+extension UIColor {
+    public class var blackHigh: UIColor {
+        return UIColor(red: 0, green: 0, blue: 0, alpha: 0.87)
+    }
+    
+    public class var blackMedium: UIColor {
+        return UIColor(red: 0, green: 0, blue: 0, alpha: 0.60)
+    }
+    
+    public class var blackLow: UIColor {
+        return UIColor(red: 0, green: 0, blue: 0, alpha: 0.30)
+    }
+
+    public class var grayLight4: UIColor {
+        return UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.0)
+    }
+    
+    // Custom color with hex value
+    public class func customColor(hex: String, alpha: CGFloat = 1.0) -> UIColor {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
