7858f2e12e8ade0a94d859427edcffd92dbf2e9c
shaival-vora <shaivalvora@gmail.com>
2023-11-25 16:36:48 -0500

    Added CommonStrings for the Login Page

--- Pennywise/Pennywise/Utility/CommonStrings.swift
+++ Pennywise/Pennywise/Utility/CommonStrings.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+// This is Class which handles all the Strings for the application
+// Define separate Enums for differnet part of the application
+struct CommonStrings {
+    // Enum Login to handle all the Login and signUp page Strings
+    enum Login: String {
+        case welcomeLabel = "Welcome Back!\nPlease Log In"
+        case signUpLabel = "Dont have an account yet? sign up"
+        case signUp = "sign up"
+        case email = "Please Enter Email ID"
+        case password = "Enter Password"
+        case login = "Log In"
+        
+        func value() -> String {
+            return self.rawValue
+        }
+        
+        
+    }
+}
