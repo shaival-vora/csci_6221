9d45db0ebfbd56077379392c8487bd8c63e813c7
shaival-vora <shaivalvora@gmail.com>
2023-11-25 20:06:02 -0500

    Sign Up Controller and Login Controller Base Changes

--- Pennywise/Pennywise/SignUpViewController.swift
+++ Pennywise/Pennywise/SignUpViewController.swift
@@ -5,4 +5,132 @@
 //  Created by Shaival Vora on 11/25/23.
 //
 
-import Foundation
+import UIKit
+/// This is the base view controller which handles the sign up page, which creates a new user account
+final class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var fullnameTextField: UITextField!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.resetData()
+        self.setUpInitialData()
+    }
+    
+    private func resetData() {
+        titleLabel.text = ""
+        subTitleLabel.text = ""
+        subTitleLabel.attributedText = nil
+        fullnameLabel.text = ""
+        fullnameTextField.text = ""
+        emailLabel.text = ""
+        emailTextField.text = ""
+        passwordLabel.text = ""
+        passwordTextField.text = ""
+        confirmPasswordLabel.text = ""
+        confirmPasswordTextField.text = ""
+        signUpButton.setTitle("", for: .normal)
+        
+    }
+    
+    private func setUpInitialData() {
+        setTitleLabel()
+        setSubTitleLabel()
+        setUpFullNameLabel()
+        setUpEmailLabel()
+        setUpPasswordLabel()
+        setUpSignUpButton()
+    }
+    
+    private func setTitleLabel() {
+        titleLabel.text = CommonStrings.SignUp.welcomeLabel.value()
+        titleLabel.textColor = UIColor.blackHigh
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        titleLabel.numberOfLines = 0
+    }
+    
+    private func setSubTitleLabel() {
+        let subtitleAttributedString: NSAttributedString = .makeClickableString(text: CommonStrings.SignUp.signInLabel.value(),
+                                                                                fontSize: 14,
+                                                                                textColor: .blackHigh,
+                                                                                targetString: CommonStrings.SignUp.signIn.value(),
+                                                                                targetColor: UIColor.redDark)
+        
+        subTitleLabel.attributedText = subtitleAttributedString
+        subTitleLabel.isUserInteractionEnabled = true // Enabling User interaction for the sign up text click action
+        subTitleLabel.lineBreakMode = .byWordWrapping
+        
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(subtitleLabelTapped(_:)))
+        subTitleLabel.addGestureRecognizer(tapGesture)
+
+    }
+    
+    @objc func subtitleLabelTapped(_ gesture: UITapGestureRecognizer) {
+        guard let title = subTitleLabel,
+              let range: NSRange = (title.text as? NSString)?.range(of: CommonStrings.SignUp.signIn.value()),
+              gesture.didTapAttributedTextInLabel(label: title, inRange: range) else {
+            return
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let destinationVC = storyboard.instantiateViewController(withIdentifier: "LoginView") as? ViewController {
+            destinationVC.modalPresentationStyle = .overFullScreen
+            dismiss(animated: true)
+        }
+    }
+    
+    private func setUpFullNameLabel() {
+        fullnameLabel.text = CommonStrings.SignUp.fullName.value()
+        fullnameLabel.font = UIFont.systemFont(ofSize: 14)
+        fullnameLabel.textColor = UIColor.blackHigh
+        fullnameTextField.placeholder = CommonStrings.SignUp.fullName.value()
+        fullnameTextField.backgroundColor = UIColor.grayLight4
+    }
+    
+    private func setUpEmailLabel() {
+        emailLabel.text = CommonStrings.SignUp.email.value()
+        emailLabel.font = UIFont.systemFont(ofSize: 14)
+        emailLabel.textColor = UIColor.blackHigh
+        emailTextField.placeholder = CommonStrings.SignUp.email.value()
+        emailTextField.backgroundColor = UIColor.grayLight4
+    }
+    
+    private func setUpPasswordLabel() {
+        passwordLabel.text = CommonStrings.SignUp.password.value()
+        passwordLabel.textColor = UIColor.blackHigh
+        passwordLabel.font = UIFont.systemFont(ofSize: 14)
+        passwordTextField.placeholder = CommonStrings.SignUp.password.value()
+        passwordTextField.backgroundColor = UIColor.grayLight4
+        
+        confirmPasswordLabel.text = CommonStrings.SignUp.confirmPassword.value()
+        confirmPasswordLabel.font = UIFont.systemFont(ofSize: 14)
+        confirmPasswordLabel.textColor = UIColor.blackHigh
+        confirmPasswordTextField.placeholder = CommonStrings.SignUp.confirmPassword.value()
+        confirmPasswordTextField.backgroundColor = UIColor.grayLight4
+    }
+    
+    private func setUpSignUpButton() {
+        signUpButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        signUpButton.setTitle(CommonStrings.SignUp.signUp.value(), for: .normal)
+        signUpButton.setTitleColor(UIColor.white, for: .normal)
+        signUpButton.layer.cornerRadius = 8
+        signUpButton.configuration?.titlePadding = 8
+        signUpButton.backgroundColor = UIColor.redDark
+    }
+    
+}
--- Pennywise/Pennywise/ViewController.swift
+++ Pennywise/Pennywise/ViewController.swift
@@ -8,12 +8,105 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameTextView: UITextView!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextView: UITextView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.resetData()
+        self.setUpInitialData()
     }
-
-
+    
+    private func resetData() {
+        titleLabel.text = ""
+        subtitleLabel.text = ""
+        subtitleLabel.attributedText = nil
+        usernameLabel.text = ""
+        passwordLabel.text = ""
+        usernameTextView.text = ""
+        passwordTextView.text = ""
+        loginButton.setTitle("",
+                             for: .normal)
+    }
+    
+    private func setRoundedCorners() {
+        usernameTextView.layer.cornerRadius = 8
+        passwordTextView.layer.cornerRadius = 8
+    }
+    
+    // setUpInitialData set the data for the initial Login Page
+    private func setUpInitialData() {
+        setUpTitleLabel()
+        setUpSubTitleLabel()
+        setUpUserNameLabel()
+        setUpPasswordLabel()
+        setUpLoginButton()
+        setRoundedCorners()
+    }
+    
+    private func setUpTitleLabel() {
+        titleLabel.text = CommonStrings.Login.welcomeLabel.value()
+        titleLabel.textColor = UIColor.blackHigh
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        titleLabel.numberOfLines = 0
+    }
+    
+    private func setUpSubTitleLabel() {
+        let subtitleAttributedString: NSAttributedString = .makeClickableString(text: CommonStrings.Login.signUpLabel.value(),
+                                                                                fontSize: 14,
+                                                                                textColor: .blackHigh,
+                                                                                targetString: CommonStrings.Login.signUp.value(),
+                                                                                targetColor: UIColor.redDark)
+        
+        subtitleLabel.attributedText = subtitleAttributedString
+        subtitleLabel.isUserInteractionEnabled = true // Enabling User interaction for the sign up text click action
+        subtitleLabel.lineBreakMode = .byWordWrapping
+        
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(subtitleLabelTapped(_:)))
+        subtitleLabel.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setUpUserNameLabel() {
+        usernameLabel.text = CommonStrings.Login.email.value()
+        usernameLabel.font = UIFont.systemFont(ofSize: 14)
+        usernameLabel.textColor = UIColor.blackMedium
+        usernameTextView.backgroundColor = UIColor.grayLight4
+    }
+    
+    private func setUpPasswordLabel() {
+        passwordLabel.text = CommonStrings.Login.password.value()
+        passwordLabel.font = UIFont.systemFont(ofSize: 14)
+        passwordLabel.textColor = UIColor.blackMedium
+        passwordTextView.backgroundColor = UIColor.grayLight4
+    }
+    
+    private func setUpLoginButton() {
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        loginButton.setTitle(CommonStrings.Login.login.value(), for: .normal)
+        loginButton.setTitleColor(UIColor.white, for: .normal)
+        loginButton.layer.cornerRadius = 8
+        loginButton.configuration?.titlePadding = 8
+        loginButton.backgroundColor = UIColor.redDark
+    }
+    
+    @objc func subtitleLabelTapped(_ gesture: UITapGestureRecognizer) {
+        guard let title = subtitleLabel,
+              let range: NSRange = (title.text as? NSString)?.range(of: CommonStrings.Login.signUp.value()),
+              gesture.didTapAttributedTextInLabel(label: title, inRange: range) else {
+            return
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let destinationVC = storyboard.instantiateViewController(withIdentifier: "SignUpView") as? SignUpViewController {
+            destinationVC.modalPresentationStyle = .overFullScreen
+            present(destinationVC, animated: true)
+        }
+    }
 }
-
