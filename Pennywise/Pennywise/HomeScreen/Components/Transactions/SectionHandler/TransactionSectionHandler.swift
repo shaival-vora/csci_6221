a58ebc0927c3ab572f131d6e312e03029cd807bb
shaival-vora <shaivalvora@gmail.com>
2023-11-27 20:25:56 -0500

    Added Files for Recent transaction section

---
+++ Pennywise/Pennywise/HomeScreen/Components/Transactions/SectionHandler/TransactionSectionHandler.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionSectionHandler.swift
+//  Pennywise
+//
+//  Created by Shaival Vora on 11/27/23.
+//
+
+import Foundation
+
+final class TransactionSectionHandler: NSObject, SectionHandler {
+    
+   
+    
+}
---
+++ Pennywise/Pennywise/HomeScreen/Components/Transactions/Views/TransactionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionTableViewCell.swift
+//  Pennywise
+//
+//  Created by Shaival Vora on 11/27/23.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
